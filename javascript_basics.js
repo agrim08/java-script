33e0268578b6f029eb9502e6310c7bbdd8d6a5fe
Agrim Gupta <agrim08>
2023-08-30 12:49:35 +0530

    JS basics

--- javascript_basics.js
+++ javascript_basics.js
@@ -1,4 +1,12 @@
 // some basic commands:
-alert("hello");
+// alert("hello");
 console.log(Math.round(2.3)); //rounding off a number
 console.log(Math.round((2096 + 977) * 0.1) / 100);
+console.log("agrim");
+let firstname = "agrim"; // declaring a variable
+console.log(firstname); //  never start a variable with an uppercase letter
+
+let $function = "harsh";
+console.log($function);
+
+let PI = 3.1415;

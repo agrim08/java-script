9fcf23a8e64713ba458663d12e830be3178f32f4
Agrim Gupta <agrim08>
2023-09-01 13:36:26 +0530

    JS data types

--- javascript_basics.js
+++ javascript_basics.js
@@ -10,3 +10,52 @@ let $function = "harsh";
 console.log($function);
 
 let PI = 3.1415;
+
+//datatypes in js:
+let javaIsFun = true;
+console.log(javaIsFun);
+console.log(typeof javaIsFun);
+
+let year;
+console.log(typeof year);
+year = 2023;
+console.log(typeof year);
+
+console.log(typeof null);
+
+// different types of variables;
+let statement = "value of this variable can be chnaged example age";
+statement = "new value";
+
+const line = "this value cannot be changed example birthyear";
+
+var old = "this type is old one and similar to let we can avoid it now";
+
+myName = "agrim";
+console.log(myName);
+
+//operators in JS:
+// arthematic operator:
+const now = 2037;
+const ageme = now - 2005;
+const ageneha = now - 1996;
+console.log(ageme, ageneha);
+console.log(ageme * 2, ageneha / 10, 2 ** 3);
+let x = 10 + 5;
+x += 10;
+x *= 4;
+x++; //x = x+1
+x--; //x = x-1
+console.log(x);
+
+//concatination:
+const firstName = "agrim";
+const lastName = "gupta";
+console.log(firstName + " " + lastName);
+
+//comparison operator;
+console.log(ageme > ageneha); // >,<, >=,<==
+console.log(ageme >= 20);
+const isFullage = ageme >= 20; // this variable is holding a boolean value
+
+console.log(now - 1996 > now - 2005);

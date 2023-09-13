5fb7e060f066a480d17ec87b53f4dbf8a42c469b
Agrim Gupta <agrim08>
2023-09-13 23:44:22 +0530

    JS ternary operator

---
+++ conditional(ternary).js
@@ -0,0 +1,10 @@
+const age = 20;
+// age >= 18
+//   ? console.log("i like to drink wine")
+//   : console.log("I like to drink water");
+
+const drink = age >= 18 ? "wine" : "water";
+console.log(drink);
+//same can be done with if else statement.
+
+console.log(`I like to drink ${age >= 18 ? "wine" : "water"}`);

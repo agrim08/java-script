c163c38f968109f5c68318061cd22cb404d99899
Agrim Gupta <agrim08>
2023-09-13 23:30:22 +0530

    JS

---
+++ switch_statements.js
@@ -0,0 +1,28 @@
+const day = "monday";
+
+switch (day) {
+  case "monday":
+    console.log("make schedule for whole week");
+    break;
+  case "tuesday":
+    console.log("JS");
+    break;
+  case "wednesday":
+    console.log("HTML");
+    break;
+  case "thursday":
+    console.log("PYTHON");
+    break;
+  case "friday":
+    console.log("C");
+    break;
+  case "satday":
+    console.log("JAVA");
+    break;
+  case "sunday":
+    console.log("SQL");
+    break;
+  default:
+    console.log("not a valid day!");
+}
+//we can use if else for the same xD

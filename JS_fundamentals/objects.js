dc60092abfb819daf876c925dd8659a5e50e3020
Agrim Gupta <agrim08>
2023-09-20 14:39:42 +0530

    Object

---
+++ JS_fundamentals/objects.js
@@ -0,0 +1,9 @@
+//objects in js is like dictionary in python
+
+const Agrim = {
+  firstname: "Agrim",
+  lastname: "Gupta",
+  job: "Student",
+  friends: ["tanishq", "Sachi", "Prashant"],
+};
+console.log(Agrim);

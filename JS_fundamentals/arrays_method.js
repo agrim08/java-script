1463eae71218661efcd5d2ad6c8034236f557004
Agrim Gupta <agrim08>
2023-09-20 12:22:43 +0530

    Arrays methods

---
+++ JS_fundamentals/arrays_method.js
@@ -0,0 +1,31 @@
+const friend = ["agrim", "ansh", "neha"];
+const newLength = friend.push("priyanshi");
+console.log(newLength);
+console.log(friend);
+
+friend.unshift("john");
+console.log(friend);
+
+//remove elements
+friend.pop(); //last
+console.log(friend);
+const popped = friend.pop();
+console.log(popped);
+console.log(friend);
+
+friend.shift(); //first
+console.log(friend);
+
+console.log(friend.indexOf("agrim"));
+console.log(friend.indexOf("mamta"));
+
+console.log(friend.includes("agrim"));
+console.log(friend.includes("mamta"));
+
+friend.push(23);
+console.log(friend.includes("23")); //does not do type coercion
+console.log(friend.includes(23));
+
+if (friend.includes("agrim")) {
+  console.log("You Have a frined called Agrim");
+}

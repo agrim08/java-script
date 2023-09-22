276b645517fe7194ec674522e9711865b2c0a494
Agrim Gupta <agrim08>
2023-09-22 21:55:51 +0530

    Object

---
+++ JS_fundamentals/dot&bracket_notation.js
@@ -0,0 +1,13 @@
+const Agrim = {
+  firstname: "Agrim",
+  lastname: "Gupta",
+  job: "Student",
+  friends: ["tanishq", "Sachi", "Prashant"],
+};
+console.log(Agrim);
+
+//dot notation:
+console.log(Agrim.lastname);
+
+//bracket notation:
+console.log(Agrim["lastname"]);

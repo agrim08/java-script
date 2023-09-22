64fa4f84ef2417fac5514bc30ae9abdbe5e66b35
Agrim Gupta <agrim08>
2023-09-22 22:04:25 +0530

    Object

--- JS_fundamentals/dot&bracket_notation.js
+++ JS_fundamentals/dot&bracket_notation.js
@@ -11,3 +11,14 @@ console.log(Agrim.lastname);
 
 //bracket notation:
 console.log(Agrim["lastname"]);
+
+const nameKey = "name";
+console.log(Agrim["first" + nameKey]);
+console.log(Agrim["last" + nameKey]);
+
+//console.log(Agrim."last" + namekey);  produce an error
+
+const knowAbt = prompt(
+  "What do you want to know abt Agrim? choose b/w firstname , lastname , age , job , friends"
+);
+console.log(Agrim[knowAbt]);

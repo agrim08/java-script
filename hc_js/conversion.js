b88f6c15027bbbff4e6b267b48c4e6a79b11df98
Agrim Gupta <agrim08>
2024-01-24 17:14:23 +0530

    js-new

---
+++ hc_js/conversion.js
@@ -0,0 +1,38 @@
+let score = "33abc";
+let test = null;
+let test2 = undefined;
+
+// console.log(typeof score);
+
+let valueInScore = Number(score);
+console.log(typeof valueInScore);
+console.log(valueInScore);
+
+let valueIntest = Number(test);
+console.log(typeof valueIntest);
+console.log(valueIntest);
+
+let valueInTest2 = Number(test2);
+console.log(typeof valueInTest2);
+console.log(valueInTest2);
+
+//"33" => 33
+//"33abc" => NaN
+
+// let isLoggedIn = 1;
+
+// let boolIsLoggedIn = Boolean(isLoggedIn);
+// console.log(isLoggedIn);
+
+let isLoggedIn = "";
+
+let boolIsLoggedIn = Boolean(isLoggedIn);
+console.log(boolIsLoggedIn);
+
+// let isLoggedIn = "Agrim";
+
+// let boolIsLoggedIn = Boolean(isLoggedIn);
+// console.log(boolIsLoggedIn);
+
+// "" => false
+// "Agrim" => true

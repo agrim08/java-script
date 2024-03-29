67878712760ea86929d27d5c8207cb07606233c1
agrim08 <agrim08>
2024-03-29 12:47:57 +0530

    JS-events

--- hc_js/projects/colorchanger/script.js
+++ hc_js/projects/colorchanger/script.js
@@ -1,25 +1,26 @@
-const buttons = document.querySelectorAll('.button');
-const body = document.querySelector('bdoy')
+const buttons = document.querySelectorAll(".button");
+const body = document.querySelector("bdoy");
 
 buttons.forEach((button) => {
-    console.log(button);
-    button.addEventListener('click', function(event){
-        console.log(event);
-        console.log(event.target);
-        if(event.target.id === 'grey'){
-            document.querySelector('.body').style.backgroundColor = event.target.id
-        }
-        if(event.target.id === 'white'){
-            document.querySelector('.body').style.backgroundColor = event.target.id
-        }
-        if(event.target.id === 'yellow'){
-            document.querySelector('.body').style.backgroundColor = event.target.id
-        }
-        if(event.target.id === 'blue'){
-            document.querySelector('.body').style.backgroundColor = event.target.id
-        }
-        if(event.target.id === 'purple'){
-            document.querySelector('.body').style.backgroundColor = event.target.id
-        }
-    })
+  console.log(button);
+  button.addEventListener("click", function (event) {
+    // event is a variable
+    console.log(event);
+    console.log(event.target);
+    if (event.target.id === "grey") {
+      document.querySelector(".body").style.backgroundColor = event.target.id;
+    }
+    if (event.target.id === "white") {
+      document.querySelector(".body").style.backgroundColor = event.target.id;
+    }
+    if (event.target.id === "yellow") {
+      document.querySelector(".body").style.backgroundColor = event.target.id;
+    }
+    if (event.target.id === "blue") {
+      document.querySelector(".body").style.backgroundColor = event.target.id;
+    }
+    if (event.target.id === "purple") {
+      document.querySelector(".body").style.backgroundColor = event.target.id;
+    }
+  });
 });

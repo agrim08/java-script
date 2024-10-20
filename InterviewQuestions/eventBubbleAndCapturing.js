d238663f7a0a18de63dd7f763a3f5293efa4a52c
agrim08 <agrim08>
2024-10-20 15:35:21 +0530

    event bubbling

---
+++ InterviewQuestions/eventBubbleAndCapturing.js
@@ -0,0 +1,26 @@
+document.getElementById("grandparent").addEventListener(
+  "click",
+  () => {
+    console.log("Grand parent Clicked");
+  },
+  true
+);
+
+document.getElementById("parent").addEventListener(
+  "click",
+  () => {
+    console.log("Parent Clicked");
+  },
+  true
+);
+
+document.getElementById("child").addEventListener(
+  "click",
+  (e) => {
+    console.log("Child Clicked");
+    e.stopPropagation();
+  },
+  true
+);
+
+//* true for CAPTURING and false for BUBBLING
